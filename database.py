12abfaba16a86f9e098eeb6ac780aaaa7c4d655c
coachhallsomeinc <justin.hall@student.awesomeinc.org>
2023-11-13 19:43:37 +0000

    adding in user auth

--- database.py
+++ database.py
@@ -1,4 +1,5 @@
 from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 SQLALCHEMY_DATABASE_URL = "postgresql://postgres:postgres@localhost:5432/postgres"
@@ -8,3 +9,12 @@ engine = create_engine(
     SQLALCHEMY_DATABASE_URL
 )
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
--- schemas.py
+++ schemas.py
@@ -1,49 +1,26 @@
 from typing import Optional
-
 from pydantic import BaseModel, EmailStr
 
-class AbilityTypeModel(BaseModel):
-    id: int
-    name: str | None
+class ProductSchema(BaseModel):
+    title: str
+    description: str | None = None
+    at_sale: bool = False
+    inventory: int
+    added_at: str
 
-class AbilityModel(BaseModel):
-    id: int
-    hero_id: int
-    ability_type_id: int
-    ability_type: AbilityTypeModel
 
-class RelationshipTypeModel(BaseModel):
-    id: int
-    name: str | None
+class User(BaseModel):
+    username: str
+    email: str
+    full_name: str | None = None
+    status: bool | None = True
 
-class RelationshipModel(BaseModel):
-    id: int
-    hero1_id: int
-    hero2_id: int
-    relationship_type_id: int
-    relationship_type: RelationshipTypeModel 
+class UserInDB(User):
+    hashed_password: str
 
-class HeroModel(BaseModel):
-    id: int
-    name: str | None
-    about_me: str | None
-    biography: str | None
-    image_url: str | None
-    abilities: list[AbilityModel]
-    enemies: list[RelationshipModel]
-    friends: list[RelationshipModel]
+class Token(BaseModel):
+    access_token: str
+    token_type: str
 
-    def __init__(self, **data):
-        super().__init__(**data)
-        self.abilities = self.abilities or []
-        self.enemies = self.enemies or []
-        self.friends = self.friends or []
-
-    class Config:
-        from_attributes = True
-
-# class ResponseModel(BaseModel):
-
-#     hero: HeroModel
-#     ability: AbilityModel
-#     ability_type: AbilityTypeModel
+class TokenData(BaseModel):
+    username: str | None = None
