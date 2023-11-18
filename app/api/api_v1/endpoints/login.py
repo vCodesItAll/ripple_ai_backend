cf669a1f3e6b9106bc25ec5a94f9f6b2d382cc22
coachhallsomeinc <justin.hall@student.awesomeinc.org>
2023-11-18 16:59:43 +0000

    initial commit of tokens controller

--- app/api/api_v1/endpoints/login.py
+++ app/api/api_v1/endpoints/login.py
@@ -34,6 +34,9 @@ def login_access_token(
     elif not controllers.user.is_active(user):
         raise HTTPException(status_code=400, detail="Inactive user")
     access_token_expires = timedelta(minutes=settings.ACCESS_TOKEN_EXPIRE_MINUTES)
+
+    # store access token in db
+    
     return {
         "access_token": security.create_access_token(
             user.id, expires_delta=access_token_expires
