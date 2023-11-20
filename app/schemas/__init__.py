07affc724ee4073556b473c5527c72c8a6a78a47
coachhallsomeinc <justin.hall@student.awesomeinc.org>
2023-11-20 00:02:54 +0000

    adding tokens refresh and create functionality

--- app/schemas/__init__.py
+++ app/schemas/__init__.py
@@ -1,3 +1,3 @@
 from .msg import Msg
-from .token import Token, TokenPayload
+from .token import TokenSchema, TokenSchemaPayload
 from .user import User, UserCreate, UserInDB, UserUpdate
--- app/schemas/token.py
+++ app/schemas/token.py
@@ -1,12 +1,9 @@
 from typing import Optional
-
 from pydantic import BaseModel
 
-
-class Token(BaseModel):
+class TokenSchema(BaseModel):
     access_token: str
     token_type: str
 
-
-class TokenPayload(BaseModel):
+class TokenSchemaPayload(BaseModel):
     sub: Optional[int] = None
